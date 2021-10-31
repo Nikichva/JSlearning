9ff0f0fcda41c125b3213bd22ee1a3de497819cd
Nikita <niki.22.12.00@gmail.com>
2021-10-31 17:40:00 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+let incr = 10,
+    decr = 10;
+
+// incr++;
+// decr--;
+
+console.log(++incr );
+console.log(--decr);
+
+console.log(5%2);
+
+console.log(2*4 === '8');
+
+const isCheked = false,
+      isClose = false;
+
+console.log(isCheked || !isClose);
+
+console.log(2 + 2 * 2 !== '6');
+
+
+ 
