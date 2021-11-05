00ce522257bcf90f8f6f8611b29b4e8a03d9f870
Nikita <niki.22.12.00@gmail.com>
2021-11-05 14:22:51 +0100

    If else switch

--- HW-1/script.js
+++ HW-1/script.js
@@ -1,19 +1,49 @@
-const NumberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+// const NumberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-const personalMovieDbB = {
-   count: NumberOfFilms,
-   movies: {},
-   actosr: {},
-   gengres: [], 
-   privat: false
-};
+// const personalMovieDbB = {
+//    count: NumberOfFilms,
+//    movies: {},
+//    actosr: {},
+//    gengres: [], 
+//    privat: false
+// };
 
-const a = prompt('Какой из последних фильмов вы смотрели?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Какой из последних фильмов вы смотрели?', ''),
-      d = prompt('На сколько оцените его?', '');
+// const a = prompt('Какой из последних фильмов вы смотрели?', ''),
+//       b = prompt('На сколько оцените его?', ''),
+//       c = prompt('Какой из последних фильмов вы смотрели?', ''),
+//       d = prompt('На сколько оцените его?', '');
 
-personalMovieDbB.movies[a] = b;
-personalMovieDbB.movies[c] = d;
+// personalMovieDbB.movies[a] = b;
+// personalMovieDbB.movies[c] = d;
 
-console.log(personalMovieDbB);
+// console.log(personalMovieDbB);
+                          
+                      //----------------УСЛОВИЯ----------------//
+// const num = 50;
+//    if (num < 49){
+//        console.log('Error');
+//    }  else if(num > 100){
+//       console.log('So much');
+//    } else {
+//       console.log('Ok');
+//    }
+
+// const num1 = 50;
+//    (num1 === 50) ? console.log('Ok!') : console.log('Error!');
+
+const num = 50;
+
+switch (num) {
+   case 49:
+      console.log('Неверно');
+      break;
+   case 100:
+      console.log('Неверно');
+      break;
+   case 50:
+      console.log('В точку!');
+      break;
+   default:
+      console.log('Не в этот раз');
+      break;         
+}
