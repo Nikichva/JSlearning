5edf8d68461ea35176e25bbe13fd5ffc292f9ce5
Nikita <niki.22.12.00@gmail.com>
2021-11-09 13:44:29 +0100

    Methods&properties of strings and numbers

--- UdemyJS/script.js
+++ UdemyJS/script.js
@@ -1,26 +1,43 @@
 "use strict";
-//--------------- FUNCTION DECLARATION ---------------//
-let num = 20;
+// //--------------- FUNCTION DECLARATION ---------------//
+// let num = 20;
 
-function showFirstMessage() {
-    console.log('Hello world!');
-    num = 10;
-}
+// function showFirstMessage() {
+//     console.log('Hello world!');
+//     num = 10;
+// }
 
-showFirstMessage();
-console.log(num);
+// showFirstMessage();
+// console.log(num);
 
-//--------------- FUNCTION EXPRESSION  ---------------//
+// //--------------- FUNCTION EXPRESSION  ---------------//
 
-const logger = function() {
-    console.log('Hello');
-};
+// const logger = function() {
+//     console.log('Hello');
+// };
 
-logger(); 
+// logger(); 
 
 
-//--------------- СТРЕЛОЧНАЯ ФУНКЦИЯ  ---------------//
-const calc = (a, b) => { 
-    console.log('1');
-    return a + b;
-}; 
+// //--------------- СТРЕЛОЧНАЯ ФУНКЦИЯ  ---------------//
+// const calc = (a, b) => { 
+//     console.log('1');
+//     return a + b;
+// }; 
+
+// const arr = [1, 2, 3];
+// console.log(arr.length);
+
+const str = "test";
+console.log(str.toUpperCase());
+
+const logg = "Здарова заебал";
+//console.log(logg.slice(7,14));
+console.log(logg.substr(8, 5));
+
+const num = 12.2;
+console.log(Math.round(num));
+
+const test = "12.5px";
+//console.log(parseInt(test));
+console.log(parseFloat(test));
